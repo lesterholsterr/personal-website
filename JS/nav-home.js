b127ce5c18563c3f9575d6425044cc2c1ab9ace4
lesterholsterr <mattyang98@gmail.com>
2023-03-15 00:47:47 -0400

    another navbar update! used DOM to paste navbar code into each page

---
+++ JS/nav-home.js
@@ -0,0 +1,27 @@
+const nav_tag = document.getElementById('navbar')
+fetch('../nav-home.html')
+  .then(res => res.text())
+  .then(data => {
+    nav_tag.innerHTML = data
+
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navlinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', ()=> {
+
+      nav.classList.toggle('nav-active');
+
+      navlinks.forEach((link, index) => {
+        if(link.style.animation) {
+          link.style.animation = '';
+        }
+        else {
+          link.style.animation = `nav-fade 0.5s ease forwards ${index/10 + 0.5}s`;
+        }
+      });
+
+      burger.classList.toggle('burger-toggle');
+
+    }); 
+  })
