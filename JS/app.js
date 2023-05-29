018c74bba20c96022497d211f0a6f66b91f354e2
lesterholsterr <mattyang98@gmail.com>
2023-05-29 19:59:23 -0400

    SEO attempt 1 + some formatting using Prettier

--- JS/app.js
+++ JS/app.js
@@ -1,25 +1,23 @@
 const navSlide = () => {
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('nav ul');
-  const navlinks = document.querySelectorAll('nav li');
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector("nav ul");
+  const navlinks = document.querySelectorAll("nav li");
 
-  burger.addEventListener('click', ()=> {
-
-    nav.classList.toggle('nav-active');
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
 
     navlinks.forEach((link, index) => {
-      if(link.style.animation) {
-        link.style.animation = '';
-      }
-      else {
-        link.style.animation = `nav-fade 0.5s ease forwards ${index/10 + 0.5}s`;
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `nav-fade 0.5s ease forwards ${
+          index / 10 + 0.5
+        }s`;
       }
     });
 
-    burger.classList.toggle('burger-toggle');
-
-  }); 
-
-}
+    burger.classList.toggle("burger-toggle");
+  });
+};
 
 navSlide();
--- JS/nav-home.js
+++ JS/nav-home.js
@@ -1,27 +1,26 @@
-const nav_tag = document.getElementById('navbar')
-fetch('../Navbar/nav-home.html')
-  .then(res => res.text())
-  .then(data => {
-    nav_tag.innerHTML = data
+const nav_tag = document.getElementById("navbar");
+fetch("../Navbar/nav-home.html")
+  .then((res) => res.text())
+  .then((data) => {
+    nav_tag.innerHTML = data;
 
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navlinks = document.querySelectorAll('.nav-links li');
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navlinks = document.querySelectorAll(".nav-links li");
 
-    burger.addEventListener('click', ()=> {
-
-      nav.classList.toggle('nav-active');
+    burger.addEventListener("click", () => {
+      nav.classList.toggle("nav-active");
 
       navlinks.forEach((link, index) => {
-        if(link.style.animation) {
-          link.style.animation = '';
-        }
-        else {
-          link.style.animation = `nav-fade 0.5s ease forwards ${index/10 + 0.5}s`;
+        if (link.style.animation) {
+          link.style.animation = "";
+        } else {
+          link.style.animation = `nav-fade 0.5s ease forwards ${
+            index / 10 + 0.5
+          }s`;
         }
       });
 
-      burger.classList.toggle('burger-toggle');
-
-    }); 
-  })
+      burger.classList.toggle("burger-toggle");
+    });
+  });
